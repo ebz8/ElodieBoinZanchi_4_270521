6ee3451cb9558418631c5dc70fab224fe6308b0e
Elodie Boin Zanchi <eboinzanchi@protonmail.com>
2021-06-05 19:08:05 +0200

    validation conditions - 1e tentative réussie

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -16,42 +16,97 @@ function editNav() {
 /////////////////
 
 // VARIABLES - DOM ELEMENTS
-// Modal block
-const modalbg = document.querySelector(".bground"); // Corps de la modale
-const modalBtn = document.querySelectorAll(".modal-btn"); // Bouton d'ouverture de la modale
-const formData = document.querySelectorAll(".formData"); // Bloc de data utilisateur
-const closeBtn = document.querySelector(".close"); // Icône x : fermeture de la modale
+// Modal blocks
+const modalCorps = document.querySelector(".bground"); // Corps de la modale
+const btnOuvrirModale = document.querySelectorAll(".modal-btn"); // Bouton d'ouverture de la modale
+const btnFermerModale = document.querySelector(".close"); // Icône x : fermeture de la modale
+const btnEnvoiFormulaire = document.getElementById("btn-submit"); // Bouton envoi du formulaire
+const formulaireChamp = document.getElementsByClassName("formData"); // Bloc contenant un input
 
 // Users data
-const formName = document.getElementById("first"); // Prénom
-const formLastName = document.getElementById("last"); // Nom
-const formMail = document.getElementById("mail"); // Adresse mail
-const formBirthdate = document.getElementById("birthdate"); // Date de naissance
-const formContestNumber = document.getElementById("number"); // Nombre de tournois
-const formSelectedCity = document.querySelectorAll(".formData .checkbox-input[type=radio]"); // Villes d'inscription
-console.log(formSelectedCity)
-// boucle for pour les locations de 1 à 6 ?
-// const formConditions = document.
+const formData = document.querySelectorAll(".formData > input"); // Bloc de data utilisateur
+// console.log(formData);
+const formPrenom = document.getElementById("first"); // Prénom
+const formNom = document.getElementById("last"); // Nom
+const formMail = document.getElementById("email"); // Adresse mail
+const formDateNaissane = document.getElementById("birthdate"); // Date de naissance
+const formConcours = document.getElementById("number"); // Nombre de tournois
+const formVille = document.querySelectorAll(".formData .checkbox-input[type=radio]"); // Villes d'inscription
+
+// Validation conditions
+// const regexName = /^(?=[a-zA-ZéèîïÉÎÏ\s]{2,25}$)(?=[a-zA-Z\s])(?:([\w\s*?])\1?(?!\1))+$/;
+// const regexMail = /\S+@\S+\.\S+/;
+// let validOrNot = boolean;
+
+const regexNom = /^(?=[a-zA-ZéèîïÉÎÏ\s]{2,25}$)(?=[a-zA-Z\s])(?:([\w\s*?])\1?(?!\1))+$/;
+const regexAdresseMail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  // birthdate:
+  // contestNumber:
+
+
 
 // MODAL EVENT
 // Launch modal
 function launchModal() {
-  modalbg.style.display = "block";
+  modalCorps.style.display = "block";
 }
 
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+btnOuvrirModale.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // Close modal
 function closeModal() {
-  modalbg.style.display = "none";
+  modalCorps.style.display = "none";
 }
 
-closeBtn.addEventListener('click', closeModal);
+btnFermerModale.addEventListener('click', closeModal);
 
-// Modal submit
-// comment sélectionner avec le for ?
-// faire un consol log pour voir comment on selectionne via label - for
+// Vérification des champs d'input
 
+// PRENOM
+function validerPrenom(){
+  if (regexNom.test(formPrenom.value) === true){
+  console.log("ok");
+
+  }else{
+    formPrenom.focus();
+
+    console.log("test d'echec");
+  }
+}
+
+// NOM
+function validerNom(){
+  if (regexNom.test(formNom.value) === true){
+  console.log("ok");
+
+  }else{
+    formNom.focus();
+    console.log("test d'echec");
+  }
+}
+
+// MAIL
+function validerMail(){
+  if (regexAdresseMail.test(formMail.value) === true){
+  console.log("ok");
+
+  }else{
+    formMail.focus();
+    console.log("test d'echec");
+  }
+}
+
+
+
+// Submit form
+function sendForm(){
+  if (formName.validity.valueMissing){
+    e.preventDefault(); 
+  }
+}
+
+btnEnvoiFormulaire.addEventListener("submit", sendForm);
 
 
 
